446d9963ecbea3b5a52adb4d0ecea5819b50b86d
barteksawicki <bartlomiej.sawicki@lamin.ar>
2024-02-17 13:35:07 +0100

    feat(CommonErrorView): add retry option

--- ui/common/src/main/kotlin/com/arakim/googlecalendarclone/ui/common/CommonErrorView.kt
+++ ui/common/src/main/kotlin/com/arakim/googlecalendarclone/ui/common/CommonErrorView.kt
@@ -1,29 +1,55 @@
 package com.arakim.googlecalendarclone.ui.common
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.arakim.googlecalendarclone.ui.common.R.string
+import com.arakim.googlecalendarclone.util.kotlin.CommonError
 
-// TODO pass common error and show info depending on it
-// TODO add retry button
+// TODO show info depending on common error
 @Composable
-fun CommonErrorView() {
-    val textColor = MaterialTheme.colorScheme.error
-    val style = MaterialTheme.typography.titleMedium.copy(color = textColor)
+fun CommonErrorView(
+    commonError: CommonError? = null,
+    onRetry: (() -> Unit)? = null
+) {
+
+    val style = MaterialTheme.typography.headlineLarge
+
+    val message = when (commonError) {
+        is CommonError.NoConnection -> string.common_error_no_connection
+        else -> string.common_error_something_went_wrong
+    }
 
     Box(
         modifier = Modifier.fillMaxSize(),
         Alignment.Center
     ) {
-        Text(
-            text = stringResource(id = string.common_error_something_went_wrong),
-            style = style
-        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+        ) {
+
+            Text(
+                text = stringResource(message),
+                style = style
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            if (onRetry != null) {
+                Button(onClick = { onRetry() }) {
+                    Text(text = stringResource(id = R.string.try_again_button_text))
+                }
+            }
+        }
     }
 }
