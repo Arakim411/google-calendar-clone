77ff5103cf25766e9d6ac05f81c38fa744ff48e1
barteksawicki <bartlomiej.sawicki@lamin.ar>
2024-02-22 13:58:01 +0100

    feat(test): add SignInUserReducerTest

--- ui/screen/sign-in/build.gradle.kts
+++ ui/screen/sign-in/build.gradle.kts
@@ -1,7 +1,7 @@
-
 import com.arakim.googlecalendarclone.dependencies.compose
 import com.arakim.googlecalendarclone.dependencies.composeHiltNavigation
 import com.arakim.googlecalendarclone.dependencies.hilt
+import com.arakim.googlecalendarclone.dependencies.jvmTests
 import com.arakim.googlecalendarclone.dependencies.libs.Google
 import com.arakim.googlecalendarclone.dependencies.mvi
 import com.arakim.googlecalendarclone.gradlebuild.plugins.AndroidComposeModulePlugin
@@ -10,6 +10,7 @@ plugins {
     id("com.android.library")
     id("com.google.dagger.hilt.android")
     id("org.jetbrains.kotlin.android")
+    id("de.mannodermaus.android-junit5") version "1.10.0.0"
     `kotlin-kapt`
 }
 
@@ -22,6 +23,7 @@ dependencies {
     implementation(Google.Api.AndroidClient)
     implementation(Google.PlayServices.Auth)
 
+    jvmTests()
     hilt()
     composeHiltNavigation()
     compose()
